9f386b0633225d0b7752e01eb74ed3ab68ec88c4
Feng.Yin <feng.yin@live.cn>
2012-04-28 21:02:36 +0800

    update with qwt change.

--- Plugins/Statistic_Invoicing/barchart.cpp
+++ Plugins/Statistic_Invoicing/barchart.cpp
@@ -60,7 +60,7 @@ void BarChart::populate()
     QList<QwtText> titles;
     titles += QString(tr("Sales Volume"));
     titles += QString(tr("Sales Quantity"));
-    d_barChartItem->setTitles(titles);
+    d_barChartItem->setBarTitles(titles);
     d_barChartItem->setLegendIconSize(QSize(10, 14));
 
     for (int i = 0; i < numBars; i++) {
